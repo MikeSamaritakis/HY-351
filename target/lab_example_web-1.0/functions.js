f750715bf3c456006c7c0e0f27a5ffd1bd6426f0
PATERAS <SOU@MANASOU.GIE.gr>
2023-05-03 12:03:05 +0300

    fix

---
+++ target/lab_example_web-1.0/functions.js
@@ -0,0 +1,9 @@
+function showLoginasAdmin(){
+    document.getElementById("loginasadmin").style.display = "block";
+    document.getElementById("loginasapplicant").style.display = "none";
+}
+
+function showLoginasApplicant(){
+    document.getElementById("loginasapplicant").style.display = "block";
+    document.getElementById("loginasadmin").style.display = "none";
+}
