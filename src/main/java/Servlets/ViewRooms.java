15709ff4815e969d7c02f6c323247590e5531db2
Michail Samaritakis <mikesamaritakis@gmail.com>
2023-05-03 12:26:51 +0300

    viewrooms started

--- src/main/java/Servlets/ViewRooms.java
+++ src/main/java/Servlets/ViewRooms.java
@@ -1,9 +1,16 @@
 package Servlets;
 
+import database.DB_Connection;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @WebServlet(name = "ViewRooms", value = "/ViewRooms")
 public class ViewRooms extends HttpServlet {
@@ -11,5 +18,38 @@ public class ViewRooms extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //applicant can see ALL rooms
+        PrintWriter out = response.getWriter();
+
+        Connection con = null;
+        try {
+            con = DB_Connection.getConnection();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        ResultSet rs;
+        String query = "select * from requests";
+
+        response.setContentType("text/html");
+        out.println("<html><body>");
+
+        try {
+            Statement stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            out.println("<table border=1 width=50% height=50%>");
+            out.println("All Requests\n");
+            while(rs.next()){
+                String reqid = rs.getString("ReqID");
+                String resid = rs.getString("ReserverIDReq");
+                String datereq = rs.getString("DateReq");
+                String roomid = rs.getString("RoomIDReq");
+                out.println("<tr><td>ID:" + reqid + "</td><td>DATE:" + datereq + "</td><td>ROOM:" + roomid + "</td><td>APPLICANT:" + resid + "</td></tr>");
+            }
+            out.println("</table>");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }catch (Exception e){
+            out.println("An error has occured.");
+        }
     }
 }
